e62d70e9ebcb01f40f6fb69fd7efc8b1f68ef44d
Artem Khoroshevskyi <tema.horoshevski@gmail.com>
2024-08-08 03:32:55 +0200

    dev

---
+++ src/ParametersBag.java
@@ -0,0 +1,13 @@
+public class ParametersBag {
+    public ParametersBag(String args[]) {
+
+    }
+
+    public  long getLimit(){
+        return  0
+;    }
+
+    public  String getPath() {
+        return  "";
+    }
+}
